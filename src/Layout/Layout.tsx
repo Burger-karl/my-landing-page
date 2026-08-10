cc078938b3cd5ad8ab3c419e645d383ee88c3fa2
Charles Kalu <charleskalu863@gmail.com>
2026-08-11 00:38:51 +0100

    fonts added and fixed build for vercel

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,27 +1,37 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import { Poppins, Urbanist } from 'next/font/google'
+import localFont from 'next/font/local'
 
-const poppins = Poppins({
-  variable: '--font-poppins',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
+const poppins = localFont({
+  src: [
+    { path: '../fonts/Poppins-Regular.ttf', weight: '400', style: 'normal' },
+    { path: '../fonts/Poppins-Medium.ttf', weight: '500', style: 'normal' },
+    { path: '../fonts/Poppins-SemiBold.ttf', weight: '600', style: 'normal' },
+    { path: '../fonts/Poppins-Bold.ttf', weight: '700', style: 'normal' },
+  ],
+  variable: '--font-heading',
+  display: 'swap',
 })
 
-const urbanist = Urbanist({
-  variable: '--font-urbanist',
-  subsets: ['latin'],
+const urbanist = localFont({
+  src: [
+    { path: '../fonts/Urbanist-Regular.ttf', weight: '400', style: 'normal' },
+    { path: '../fonts/Urbanist-Medium.ttf', weight: '500', style: 'normal' },
+  ],
+  variable: '--font-body',
+  display: 'swap',
 })
 
 type LayoutProps = {
   children: React.ReactNode
+  hideNavbar?: boolean
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, hideNavbar }: LayoutProps) => {
   return (
-    <div className={`${poppins.variable} ${urbanist.variable} min-h-screen flex flex-col bg font-body`}>
-      <Navbar />
+    <div className={`${poppins.variable} ${urbanist.variable} min-h-screen flex flex-col bg-white font-body`}>
+      {!hideNavbar && <Navbar />}
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>
